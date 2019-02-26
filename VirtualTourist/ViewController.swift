296694cf50a649be389d47ec6a77dda37d16ee64
“Zeyad” <zeyad888@outlook.com>
2019-02-26 22:03:00 +0300

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Zeyad AlHusainan on 26/02/2019.
+//  Copyright © 2019 Zeyad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
